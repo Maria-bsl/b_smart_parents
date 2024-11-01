58e100631ad0e4941ee011bcbdc6340eb9275021
Maria-bsl <maria@bizlogicsolutions.com>
2024-11-01 08:45:29 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'b_smart_parents',
+  webDir: 'www'
+};
+
+export default config;
